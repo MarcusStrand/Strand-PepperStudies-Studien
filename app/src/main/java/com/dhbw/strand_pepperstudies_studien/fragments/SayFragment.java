628e0faf17d34a9ea9ae5deeb972849385a40559
Nico Wolff <nico.wolff@kardex.com>
2022-04-21 14:45:48 +0200

    working say fragment screen

--- app/src/main/java/com/dhbw/strand_pepperstudies_studien/fragments/SayFragment.java
+++ app/src/main/java/com/dhbw/strand_pepperstudies_studien/fragments/SayFragment.java
@@ -7,16 +7,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.object.conversation.Say;
+import com.dhbw.strand_pepperstudies_studien.MainActivity;
 import com.dhbw.strand_pepperstudies_studien.R;
+import com.dhbw.strand_pepperstudies_studien.activities.SayActivity;
 
 public class SayFragment extends Fragment implements RobotLifecycleCallbacks {
 
     private static final String TAG = "PepperStudies_SayFragment";
     QiContext qiContext;
+    View view;
 
+    Button button_Explanation;
+    Button button_Say;
+
+    SayActivity sayActivity;
 
     // Android Lifecycle Callbacks
 
@@ -26,6 +35,9 @@ public class SayFragment extends Fragment implements RobotLifecycleCallbacks {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
+        sayActivity = new SayActivity();
+
         super.onCreate(savedInstanceState);
     }
 
@@ -33,7 +45,12 @@ public class SayFragment extends Fragment implements RobotLifecycleCallbacks {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_say, container, false);
+        view = inflater.inflate(R.layout.fragment_say, container, false);
+        MainActivity ma = (MainActivity) getActivity();
+        this.qiContext = ma.getQiContext();
+
+        initializeButtonsAndOnClickListeners();
+        return view;
     }
 
     @Override
@@ -51,17 +68,25 @@ public class SayFragment extends Fragment implements RobotLifecycleCallbacks {
     public void onRobotFocusRefused(String reason) {
         Log.i(TAG, "Robot focus refused because " + reason +  " " + TAG);
     }
-/*
+
     public void initializeButtonsAndOnClickListeners()
     {
         if(qiContext != null)
         {
             button_Explanation = view.findViewById(R.id.button_Explanation);
+            button_Say = view.findViewById(R.id.button_Say);
 
             button_Explanation.setOnClickListener(v -> {
                 if (qiContext != null) {
                     sayActivity.setQiContext(this.qiContext);
-                    sayActivity.SaySomething("This button should explain the home screen to you, but it does not");
+                    sayActivity.SaySomething("This button should explain the say screen to you, but it does not");
+                }
+            });
+
+            button_Say.setOnClickListener(v -> {
+                if (qiContext != null) {
+                    sayActivity.setQiContext(this.qiContext);
+                    sayActivity.SaySomething("She sells sea shells by the sea shore.");
                 }
             });
         }
@@ -70,5 +95,5 @@ public class SayFragment extends Fragment implements RobotLifecycleCallbacks {
             Log.i(TAG,"QiContext is null! " + TAG);
         }
     }
-*/
+
 }
